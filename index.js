db6230b73aef323ecb8713497add275fa7a87f11
rudra-123-patel <76771155+rudra-123-patel@users.noreply.github.com>
2023-05-25 14:13:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious&type=single"; 
+
+
+let getJoke = () => {
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item =>{
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
+
+}
+btn.addEventListener("click",getJoke); 
+getJoke();
